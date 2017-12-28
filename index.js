79f9f22069d567033bf7bec54abb8deb6be78f67
Joseph Epifano <joepifano@gmail.com>
2017-12-28 04:42:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ function dec(n) {
   return (n - 1)
 }
 function makeInt(n) {
-  parseInt("2", 10)
+  parseInt('n', 10)
   return parseInt(n)
 }
 function preserveDecimal(n) {
   return parseFloat(n)
 }
+
 
 
 
